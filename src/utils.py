5032fa40d99ea31a8d172a2fa6ad8a87e050c29f
DOOHAE JUNG <doohae@DOOHAE-M1-Pro.local>
2022-03-31 01:44:21 +0900

    utils.py

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,16 @@ import torch
 import random
 import numpy as np
 
+from typing import NoReturn
 
-def set_seed(random_seed):
+
+def set_seed(random_seed: int=42)->NoReturn:
     torch.manual_seed(random_seed)
     torch.cuda.manual_seed(random_seed)
     torch.cuda.manual_seed_all(random_seed)  # if use multi-GPU
     random.seed(random_seed)
     np.random.seed(random_seed)
+
+
+def get_device()->str:
+    return "cuda" if torch.cuda.is_available() else "cpu"
