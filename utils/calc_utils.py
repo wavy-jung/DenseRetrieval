7e3e7578b784239b6f784f94371e1e504af1b218
Doohae <rick7213@gmail.com>
2022-06-11 20:21:44 +0900

    <docs>: calculate_mrr moved calculate_mrr functions moved from train.py to calc_utils.py

--- utils/calc_utils.py
+++ utils/calc_utils.py
@@ -11,3 +11,22 @@ def matrix_mul(query_tensor: T, ctx_tensor: T) -> T:
 def cosine_sim(query_tensor: T, ctx_tensor: T):
     tensor_mul = matrix_mul(query_tensor, ctx_tensor)
     pass
+
+def calculate_mrr(indices: torch.LongTensor, targets: torch.LongTensor) -> float:
+        """
+        Calculates the MRR score for the given predictions and targets
+        Args:
+            indices (Bxk): torch.LongTensor. top-k indices predicted by the model.
+            targets (B): torch.LongTensor. actual target indices.
+
+        Returns:
+            mrr (float): the mrr score
+        """
+        tmp = targets.view(-1, 1)
+        targets = tmp.expand_as(indices)
+        hits = (targets == indices).nonzero()
+        ranks = hits[:, -1] + 1
+        ranks = ranks.float()
+        rranks = torch.reciprocal(ranks)
+        mrr = torch.sum(rranks).data / targets.size(0)
+        return mrr.item() 
